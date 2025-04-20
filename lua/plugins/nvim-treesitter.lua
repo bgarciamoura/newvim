555232d5c5bf0727520b8a62e433d187e00ceb97
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-04-20 15:25:23 -0300

    feat: Included nvim-treesitter

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,29 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+				"typescript",
+				"html",
+				"css",
+				"json",
+				"markdown",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					node_decremental = "<BS>",
+				},
+			},
+		})
+	end,
+}
