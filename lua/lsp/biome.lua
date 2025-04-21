9797cd4a3e773acc10095591e0d3d7ba64c1b6c3
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-04-20 23:27:20 -0300

    feat: Included prettierd for format html files

--- lua/lsp/biome.lua
+++ lua/lsp/biome.lua
@@ -1,23 +1,21 @@
-vim.lsp.config['biome'] = {
-  cmd = { 'biome', 'lsp-proxy' },
-  filetypes = {
-    'astro',
-    'css',
-    'graphql',
-    'javascript',
-    'javascriptreact',
-    'json',
-    'jsonc',
-    'svelte',
-    'typescript',
-    'typescript.tsx',
-    'typescriptreact',
-    'vue',
-    'htmlangular',
-  },
-  root_markers = { 'biome.json', 'biome.jsonc' },
-  single_file_support = false,
+vim.lsp.config["biome"] = {
+	cmd = { "biome", "lsp-proxy" },
+	filetypes = {
+		"astro",
+		"css",
+		"graphql",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"jsonc",
+		"svelte",
+		"typescript",
+		"typescript.tsx",
+		"typescriptreact",
+		"vue",
+		"htmlangular",
+	},
+	root_markers = { "biome.json", "biome.jsonc" },
 }
 
 vim.lsp.enable("biome")
-
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,12 +1,34 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			html = { "prettierd" },
+			htmlangular = { "prettierd" },
+			javascript = { "biome" },
+			javascriptreact = { "biome" },
+			typescript = { "biome" },
+			typescriptreact = { "biome" },
+			css = { "prettierd" },
+			json = { "biome" },
+		},
+		formatters = {
+			biome = {
+				command = "biome",
+				args = { "format", "--stdin-file-path", "$FILENAME" },
+				stdin = true,
+				require_cwd = true,
+			},
+			prettierd = {
+				command = "prettierd",
+				args = { "$FILENAME" },
+				stdin = true,
+				require_cwd = true,
+			},
+		},
+		format_on_save = {
+			timeout_ms = 3000,
+			lsp_format = "fallback",
+		},
+	},
 }
