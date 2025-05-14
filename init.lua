88365da1783e850f3e80013f848de116fa10eb09
Bruno Garcia <itsme@bgarciamoura.com>
2025-05-14 12:45:13 -0300

    feat: Adjusted mason to auto install some packages

--- init.lua
+++ init.lua
@@ -6,10 +6,6 @@ require("core.diagnostics")
 require("core.autocmds")
 require("core.lazy")
 
-
--- Anterior: Carregar capacidades LSP e busca dos arquivos .lua no diretório LSP
--- Agora Mason + LSPConfig irá gerenciar isso, então essas linhas podem ser removidas
-
 -- Mantendo inlay hints (o plugin LSP irá configurar isso quando cada servidor se conectar)
 vim.lsp.inlay_hint.enable()
 vim.cmd("colorscheme kanagawa")
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,16 +2,30 @@ local spec = {
     "mason-org/mason.nvim",
 }
 function spec.config()
-local mason =    require("mason")
-  mason.setup()
+    local mason = require("mason")
+    local mason_registry = require("mason-registry")
+    
+    mason.setup()
+    
     local packages = {
-      luals = "lua-language-server",
-      bashls = "bash-language-server",
-      biome = "biome",
-      cssls = "css-lsp"
+        "lua-language-server",
+        "bash-language-server",
+        "biome",
+        "css-lsp",
+        "html-lsp",
+        "emmet-language-server",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "json-lsp",
+        "typescript-language-server",
+        "prettier",
+        "eslint_d"
     }
-    for cmd, pkg in pairs(packages) do
-        if vim.fn.executable(cmd) == 0 then
+    
+    for _, pkg in ipairs(packages) do
+        -- Verifica se o pacote já está instalado
+        if not mason_registry.is_installed(pkg) then
+            -- Instala o pacote se ele não estiver instalado
             vim.cmd("MasonInstall " .. pkg)
         end
     end
