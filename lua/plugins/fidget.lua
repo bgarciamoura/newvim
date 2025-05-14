b584967ed4ef51bc69121651d9319c667e040f6e
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-05-14 00:23:19 -0300

    refact: Changed the way I'm using LSP servers

--- lua/plugins/fidget.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "j-hui/fidget.nvim",
-  priority = 10000,
-  opts = {
-    notification = {
-      override_vim_notify = true,
-    },
-  },
-}
--- lua/plugins/mason.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "williamboman/mason.nvim",
-  priority = 1001,
-  config = function()
-    require("mason").setup({})
-  end
-}
