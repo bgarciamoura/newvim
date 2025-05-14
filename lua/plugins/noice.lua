ee4b32b234ccfffef5224de9ffbfabe02fa82300
Bruno Garcia <itsme@bgarciamoura.com>
2025-05-14 13:52:47 -0300

    feat: Included Noice for better cmd UI

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,27 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		cmdline = {
+			enabled = true,
+			view = "cmdline_popup",
+			opts = {},
+			format = {
+				cmdline = { pattern = "^:", icon = "", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+				input = { view = "cmdline_input", icon = "󰥻 " },
+			},
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		-- "rcarriga/nvim-notify",
+	},
+}
