8325f78e497ba4ab68768811319d784ba1a1a79e
Bruno Garcia <itsme@bgarciamoura.com>
2025-05-21 17:42:30 -0300

    feat: Included keymaps for codecompanion

---
+++ lua/core/keymaps/codecompanion_spec.lua
@@ -0,0 +1,40 @@
+return {
+	mappings = {
+		{
+			"n",
+			"<leader>cc",
+			"<cmd>CodeCompanion<CR>",
+			"Code Companion",
+		},
+		{
+			"n",
+			"<leader>cC",
+			"<cmd>CodeCompanionClose<CR>",
+			"Close Code Companion",
+		},
+		{
+			"n",
+			"<leader>cF",
+			"<cmd>CodeCompanionFormat<CR>",
+			"Format Code Companion",
+		},
+		{
+			"n",
+			"<leader>cT",
+			"<cmd>CodeCompanionChat Toggle<CR>",
+			"Chat Code Companion",
+		},
+		{
+			"n",
+			"<leader>cA",
+			"<cmd>CodeCompanionChat Add<CR>",
+			"Add Code to Companion Chat",
+		},
+		{
+			"n",
+			"<leader>cD",
+			"<cmd>CodeCompanionCmd<CR>",
+			"Code Command Code Companion",
+		},
+	},
+}
