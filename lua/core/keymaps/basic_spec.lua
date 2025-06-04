0fc5996124c03a5eda541270ee5f8e5962845a4c
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-06-04 14:50:56 -0300

    feat: Added a keymap to duplicate line and comment the original one

--- lua/core/keymaps/basic_spec.lua
+++ lua/core/keymaps/basic_spec.lua
@@ -5,5 +5,6 @@ return {
 		{ "n", "<C-q>", "<Cmd>q<CR>", "Sair" },
 		{ "n", "ZZ", "<Cmd>x<CR>", "Salvar e sair" },
 		{ "n", "<C-C>", "<Cmd>let @/ = ''<CR>", "Limpar padrão de busca", { nowait = true } },
+		{ "n", "ycc", "yygccp", "Copiar e comentar linha, depois colar abaixo", { remap = true } },
 	},
 }
