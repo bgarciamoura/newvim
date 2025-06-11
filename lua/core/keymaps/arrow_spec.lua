ebfa414869ed7c9846743e99d649ceed68b4f3fa
Bruno Garcia Moura <bgarciamoura@gmail.com>
2025-06-11 19:31:54 -0300

    feat: add arrow navigation plugin

---
+++ lua/core/keymaps/arrow_spec.lua
@@ -0,0 +1,10 @@
+return {
+    group_name = "Arrow",
+    group_prefix = "<leader>a",
+    mappings = {
+        { "n", "<leader>aa", "<cmd>ArrowMark<CR>", "Adicionar marca Arrow" },
+        { "n", "<leader>al", "<cmd>ArrowMenu<CR>", "Abrir menu Arrow" },
+        { "n", "<leader>an", "<cmd>ArrowNext<CR>", "Próxima marca" },
+        { "n", "<leader>ap", "<cmd>ArrowPrev<CR>", "Marca anterior" },
+    },
+}
