0f63acbd5d5f6b0cb46bc809c7adcc39bce3a818
Bruno Garcia Moura <bgarciamoura@gmail.com>
2025-06-20 11:50:12 -0300

    Add keymaps for second terminal

--- lua/core/keymaps/toggleterm_spec.lua
+++ lua/core/keymaps/toggleterm_spec.lua
@@ -1,7 +1,32 @@
 return {
 	group_name = "ToggleTerm",
 	group_prefix = "<leader>T",
-	mappings = {
-		{ "n", "<leader>tt", "<cmd>ToggleTerm<CR>", "Abrir Terminal" },
-	},
+        mappings = {
+                { "n", "<leader>tt", "<cmd>ToggleTerm<CR>", "Abrir Terminal" },
+                {
+                        "n",
+                        "<leader>tv",
+                        "<cmd>ToggleTerm direction=vertical<CR>",
+                        "Abrir Terminal Vertical",
+                },
+                {
+                        "n",
+                        "<leader>th",
+                        "<cmd>ToggleTerm direction=horizontal<CR>",
+                        "Abrir Terminal Horizontal",
+                },
+                { "n", "<leader>t2", "<cmd>2ToggleTerm<CR>", "Abrir Segundo Terminal" },
+                {
+                        "n",
+                        "<leader>t2v",
+                        "<cmd>2ToggleTerm direction=vertical<CR>",
+                        "Segundo Terminal Vertical",
+                },
+                {
+                        "n",
+                        "<leader>t2h",
+                        "<cmd>2ToggleTerm direction=horizontal<CR>",
+                        "Segundo Terminal Horizontal",
+                },
+        },
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,11 +3,11 @@ return {
 	version = "*",
 	event = "VeryLazy",
 	config = function()
-	require("toggleterm").setup({
-		size = 20,
-		open_mapping = [[<c-\>]],
-		shade_terminals = true,
-		direction = "float",
-	})
+        require("toggleterm").setup({
+                size = 20,
+                open_mapping = [[<c-\>]],
+                shade_terminals = true,
+                direction = "horizontal",
+        })
 	end,
 }
