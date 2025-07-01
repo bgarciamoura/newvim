4e7286fd751e971ed5d50fd5673298355dbb71c6
Bruno Garcia <itsme@bgarciamoura.com>
2025-07-01 10:24:37 -0300

    feat: Included Claude Code

---
+++ lua/core/keymaps/claude_spec.lua
@@ -0,0 +1,7 @@
+return {
+	group_name = "Claude",
+	group_prefix = "<leader>c",
+	mappings = {
+		{ "n", "<leader>CC", "<Cmd>ClaudeCode<CR>", "Abrir o Claude Code" },
+	},
+}
--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -2,6 +2,8 @@ return {
 	"ahmedkhalf/project.nvim",
 	config = function()
 		require("telescope").load_extension("projects")
-		require("project_nvim").setup({})
+		require("project_nvim").setup({
+			manual_mode = true,
+		})
 	end,
 }
