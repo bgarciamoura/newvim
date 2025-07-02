ce7b646e6ed3f4604040146fd80dc7b901a1a597
Bruno Garcia <itsme@bgarciamoura.com>
2025-07-02 13:58:23 -0300

    feat: Removed codecompanion

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = { "MunifTanjim/nui.nvim" },
 	config = function()
 		require("hardtime").setup({
+			enabled = false,
 			max_count = 5,
 			max_time = 1000,
 			disable_filetypes = { "TelescopePrompt", "NvimTree", "neo-tree" },
