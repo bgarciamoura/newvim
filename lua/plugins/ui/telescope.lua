2caa0e582deb3739db9a95049cd4f8e74ca4a093
Bruno Garcia Moura <itsme@bgarciamoura.com>
2025-08-27 10:51:18 -0300

    feat: Included a shortcut to live grep with telescope

--- lua/plugins/ui/telescope.lua
+++ lua/plugins/ui/telescope.lua
@@ -23,6 +23,8 @@ return {
     { "<leader>/", function() require("telescope.builtin").live_grep() end, desc = "Grep (Root Dir)" },
     { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
     { "<leader><space>", function() require("telescope.builtin").find_files() end, desc = "Find Files (Root Dir)" },
+    -- Live grep shortcut
+    { "<leader>lg", function() require("telescope.builtin").live_grep() end, desc = "Live Grep" },
     -- find
     { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
     { "<leader>fc", function() require("telescope.builtin").find_files() end, desc = "Find Config File" },
